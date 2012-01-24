a3a63d1de0b3a93a6b38285b8e1803f8d583ac82
ben <ben@nerdlabor.de>
2012-01-24 18:36:58 +0100

    added back german menu

--- app/helpers/wiki_helper.rb
+++ app/helpers/wiki_helper.rb
@@ -24,69 +24,69 @@ module WikiHelper
   end
 
   def edit_page
-    link_text = (@page.name == "HomePage" ? 'Edit Page' : 'Edit')
-    link_to(link_text, {:web => @web.address, :action => 'edit', :id => @page.name}, 
+    link_text = (@page.name == "HomePage" ? 'Seite bearbeiten' : 'Bearbeiten')
+    link_to(link_text, {:web => @web.address, :action => 'edit', :id => @page.name},
         {:class => 'navlink', :accesskey => 'E', :id => 'edit', :rel => 'nofollow'})
   end
 
   def edit_web
-    link_to('Edit Web', {:web => @web.address, :action => 'edit_web'}, 
+    link_to('Web bearbeiten', {:web => @web.address, :action => 'edit_web'},
         {:class => 'navlink', :accesskey => 'W', :id => 'edit_web', :rel => 'nofollow'})
   end
 
   def history
-    link_to_history(@page, 'History',
+    link_to_history(@page, 'Versionsgeschichte',
         {:class => 'navlink', :accesskey => 'S', :id => 'history', :rel => 'nofollow'})
   end
-            
+
   def forward
     if @revision_number < @page.rev_ids.size - 1
-      link_to('Forward in time', 
+      link_to('Vorwärts',
           {:web => @web.address, :action => 'revision', :id => @page.name, :rev => @revision_number + 1},
-          {:class => 'navlink', :accesskey => 'F', :id => 'to_next_revision', :rel => 'nofollow'}) + 
-          " <span class='revisions'>(#{@revision.page.rev_ids.size - @revision_number} more)</span> ".html_safe
+          {:class => 'navlink', :accesskey => 'F', :id => 'to_next_revision', :rel => 'nofollow'}) +
+          " <span class='revisions'>(#{@revision.page.revisions.size - @revision_number} more)</span> ".html_safe
     else
-        link_to('Forward in time', {:web => @web.address, :action => 'show', :id => @page.name},
+        link_to('Vorwärts', {:web => @web.address, :action => 'show', :id => @page.name},
             {:class => 'navlink', :accesskey => 'F', :id => 'to_next_revision', :rel => 'nofollow'}) +
-            " <span class='revisions'>(to current)</span>".html_safe
+            " <span class='revisions'>(zur aktuellen Version)</span>".html_safe
     end
   end
-    
+
   def back_for_revision
-    link_to('Back in time',
+    link_to('Zurück',
         {:web => @web.address, :action => 'revision', :id => @page.name, :rev => @revision_number - 1},
-        {:class => 'navlink', :id => 'to_previous_revision', :rel => 'nofollow'}) + 
+        {:class => 'navlink', :id => 'to_previous_revision', :rel => 'nofollow'}) +
         " <span class='revisions'>(#{@revision_number - 1} more)</span>".html_safe
   end
 
   def back_for_page
-    link_to('Back in time', 
-        {:web => @web.address, :action => 'revision', :id => @page.name, 
+    link_to('Zurück',
+        {:web => @web.address, :action => 'revision', :id => @page.name,
         :rev => @page.rev_ids.size - 1},
         {:class => 'navlink', :accesskey => 'B', :id => 'to_previous_revision', :rel => 'nofollow'}) +
-        " <span class='revisions'>(#{@page.rev_ids.size - 1} #{@page.rev_ids.size - 1 == 1 ? 'revision' : 'revisions'})</span>".html_safe
+        " <span class='revisions'>(#{@page.revisions.size - 1} #{@page.revisions.size - 1 == 1 ? 'Revision' : 'Revisionen'})</span>".html_safe
   end
-  
+
   def current_revision
-    link_to('See current', {:web => @web.address, :action => 'show', :id => @page.name},
+    link_to('Aktuelle Version anzeigen', {:web => @web.address, :action => 'show', :id => @page.name},
         {:class => 'navlink', :id => 'to_current_revision'})
   end
-  
+
   def see_or_hide_changes_for_revision
-    link_to(@show_diff ? 'Hide changes' : 'See changes', 
-        {:web => @web.address, :action => 'revision', :id => @page.name, :rev => @revision_number, 
+    link_to(@show_diff ? 'Änderungen verbergen' : 'Änderungen anzeigen',
+        {:web => @web.address, :action => 'revision', :id => @page.name, :rev => @revision_number,
          :mode => (@show_diff ? nil : 'diff') },
         {:class => 'navlink', :accesskey => 'C', :id => 'see_changes', :rel => 'nofollow'})
   end
 
   def see_or_hide_changes_for_page
-    link_to(@show_diff ? 'Hide changes' : 'See changes', 
+    link_to(@show_diff ? 'Änderungen verbergen' : 'Änderungen anzeigen',
         {:web => @web.address, :action => 'show', :id => @page.name, :mode => (@show_diff ? nil : 'diff') },
         {:class => 'navlink', :accesskey => 'C', :id => 'see_changes', :rel => 'nofollow'})
   end
-  
+
   def rollback
-    link_to('Rollback', 
+    link_to('Auf diese Version zurücksetzen',
         {:web => @web.address, :action => 'rollback', :id => @page.name, :rev => @revision_number},
         {:class => 'navlink', :id => 'rollback', :rel => 'nofollow'})
   end
